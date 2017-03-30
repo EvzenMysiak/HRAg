a4b6c7037b44128a01f1eba49bb6a33d19713727
Michal Janosik <miso.janosik@gmail.com>
2017-03-30 16:17:27 +0200

    uvod

--- main.c
+++ main.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include <math.h>
+
+typedef struct centralny_sklad {
+    int jedlo;
+    int pivo;
+    int ruda;
+    int mince;
+};
+
+typedef struct moj_hrac {
+    char meno[20];
+    char nik[20];
+};
+
+void credits(){
+    printf("Create by Michal Janosik\n");
+    printf("Zadanie 1 - Hra");
+}
+
+void welcome(){
+    printf("Vitajte v mojej konzolovej aplikacii HRAq\n");
+    printf("Hra je odvodena od hry Dwarf Fortress\n");
+    printf("Podme teda hrat\n");
+}
 
 int main() {
-    printf("Hello, World!\n");
-    printf("hellokok");
+    credits();
+    welcome();
     return 0;
 }
